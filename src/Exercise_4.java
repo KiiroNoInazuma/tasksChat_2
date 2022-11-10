f0828a6aab5fb6ee6d5913d66c5d594355269ede
John Rey <adioskid@mail.ru>
2022-11-10 23:28:28 +0300

    Homework#5

--- src/Exercise_4.java
+++ src/Exercise_4.java
@@ -1,10 +1,10 @@
 public class Exercise_4 {
     public static void main(String[] args) {
         int[] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
-        for (int j : arr) {
-            for (int i : arr) {
-                if (j + i == -2)
-                    System.out.println("[" + j + "] " + "[" + i + "]");
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int i = j + 1; i < arr.length; i++) {
+                if (arr[j] + arr[i] == -2)
+                    System.out.println("[" + arr[j] + "] " + "[" + arr[i] + "]");
             }
         }
     }
